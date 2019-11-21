de3216bb17337c837ff540e1b6815e682e9662e6
Bart <bart.claessens@hotmail.com>
2019-11-21 14:05:39 +0100

    margins check

--- src/main/scala/sless/ast/Lint.scala
+++ src/main/scala/sless/ast/Lint.scala
@@ -24,7 +24,28 @@ trait Lint extends LintDSL with Base {
     * the single shorthand property margin. The new margin property takes the place of the first declaration in order of appearance.
     * The values from the individual prorperties are aggregated in the order top-right-bottom-left, with spaces in between.
     */
-  override def aggregateMargins(css: CssAST): (Boolean, CssAST) = ???
+  override def aggregateMargins(css: CssAST): (Boolean, CssAST) =
+      ( css.rules.exists(hasAllMargins)  , CssAST(Nil) )
+
+    def hasAllMargins(rule: RuleAST) = hasAllProperties(rule, marginList)
+
+    def hasAllProperties(rule: RuleAST, props: List[String]): Boolean =
+      props.map(p=>hasProperty(rule,p)).fold(true)(_ && _)
+
+    def hasProperty(rule: RuleAST, prop: String): Boolean = rule match {
+      case ARule(_,Nil) => false
+      case CommentRule(r,comment) => hasProperty(r,prop)
+      case ARule(s, declarations) => declarations.exists(d=>hasProperty(d,prop))
+    }
+
+    def hasProperty(declaration: DeclarationAST, prop: String): Boolean = declaration match {
+//      case ADeclaration(Nil,_)  => false
+      case CommentDeclaration(d,comment) => hasProperty(d,prop)
+      case ADeclaration(AProperty(pvalue), dvalue) => pvalue.equals(prop)
+    }
+
+
+    val marginList = List("margin-top", "margin-right", "margin-bottom", "margin-left")
 
 
   /**
