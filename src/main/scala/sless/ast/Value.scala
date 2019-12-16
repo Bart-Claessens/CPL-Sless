79eebc070897ecf5b7694522367031f991c6f8d3
Bart <v>
2019-12-16 19:12:10 +0100

    Mixins and tests

--- src/main/scala/sless/ast/Value.scala
+++ src/main/scala/sless/ast/Value.scala
@@ -4,4 +4,6 @@ import sless.dsl.ValueDSL
 
 trait Value extends ValueDSL with Base {
   override def value(string: String): Value = AValue(string)
+
+  def value(int: Int): Value = AValue(int.toString)
 }
