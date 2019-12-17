52963241d5fe1c8defccbfd079a96dc67614a673
Bart <bart.claessens@hotmail.com>
2019-12-17 20:45:56 +0100

    Merge v2 Vanilla works

--- src/main/scala/sless/ast/Merge.scala
+++ src/main/scala/sless/ast/Merge.scala
@@ -3,37 +3,21 @@ package sless.ast
 import sless.dsl.MergeDSL
 
 trait Merge extends MergeDSL with Base with Property with Value with Rule with Selector {
-  override def mergeSheets(cssSheets: Css*): Css = css(mergeRules(cssSheets.map(css=>css.getRules).flatten):_*)
+  override def mergeSheets(cssSheets: Css*): Css =
+    css(cssSheets.map(_.getRules).foldRight(Seq():Seq[Rule])(mergeRules):_*)
 
-  //css( cssSheets.map(_.getRules).fold(Seq())((r1,r2)=>mergeHelper(r1,r2)) :_* )
-//    css(mergeHelper(cssSheets.map(c=>c.getRules)):_*)
 
-//  def mergeHelper(ruleSeqs: Seq[Seq[Rule]]): Seq[Rule] = ???
-
-//  def mergeHelper(rules1: Seq[Rule], rules2: Seq[Rule]): Seq[Rule] = rules1 match {
-//    case Seq() => rules2
-//    case Seq(first, tail @ _*) => mergeHelper(tail, merger(first,rules2 ))
-//  }
-//
-//  def merger(rule1: Rule, rules2: Seq[Rule]): Seq[Rule] = {
-//    val indexr2 = rules2.indexWhere( r=>equals(r.getSelector,rule1.getSelector))
-//    if (indexr2 == -1) rule1 +: rules2 else {
-//      val r2 = rules2.take(indexr2)
-//    }
-//  }
-  def mergeRules(rules: Seq[Rule]): Seq[Rule] = mergeHelper(rules, Seq())._2
-
-  def mergeHelper(rules: Seq[Rule], newrules: Seq[Rule]): (Seq[Rule],Seq[Rule]) = rules match {
-    case Seq() => (rules,newrules)
-    case Seq(rule) => (Seq() ,  rule +: newrules)
+  def mergeRules(rules1: Seq[Rule], rules2: Seq[Rule]): Seq[Rule] = rules1 match {
+    case Seq() => rules2
+    case Seq(rule) => rule +: rules2
     case _ => {
-      val rule1 = rules.last
+      val rule1 = rules1.last
       rule1.getSelector match {
-        case UniversalSelector => (rules.dropRight(1), newrules)
+        case UniversalSelector => mergeRules(rules1.dropRight(1), rules2)
         case _ => {
-          val (rs,ds) = rules.dropRight(1).map(mergeMapp(_,rule1)).unzip
+          val (rs,ds) = rules1.dropRight(1).map(mergeMapp(_,rule1)).unzip
           val rule2 = ds.foldRight(rule1)( addUniqueDeclarationsToRule)
-          mergeHelper(rs ,  rule2 +: newrules)
+          mergeRules(rs ,  rule2 +: rules2)
         }
       }
     }
@@ -51,11 +35,4 @@ trait Merge extends MergeDSL with Base with Property with Value with Rule with S
     }
   }
 
-   //mergeHelper(rules1 :+ first, tail)
-
-//  def mergeHelper(rules1: Seq[Rule], rules2: Seq[Rule]): Seq[Rule] = for (rule1 <- rules1) {
-//    for (rule2 <- rules2) {
-//      if (rule1.getSelector.equals(rule2.getSelector)) rules2:+rule1
-//    }
-//  }
 }
